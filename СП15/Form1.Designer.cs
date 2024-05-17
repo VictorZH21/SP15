c16e099daad9af9f9d63f5bf6a9ddd352cc0d91a
Coffey <H1.coffey@bk.ru>
2024-05-17 23:34:05 +0700

    15

---
+++ СП15/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace СП15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(285, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DRIVE:";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(290, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(848, 381);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(262, 438);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "0");
+            this.imageList1.Images.SetKeyName(1, "1");
+            this.imageList1.Images.SetKeyName(2, "2");
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(994, 436);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 53);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Выход";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(821, 436);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 53);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(649, 436);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 53);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Переименовать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(469, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 53);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Создать текстовый файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(290, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 53);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Новая папка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 544);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ СП15/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace СП15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+        private void LoadDrives()
+        {
+            try
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                foreach (var drive in drives)
+                {
+                    if (drive.IsReady)
+                    {
+                        TreeNode treeNode = treeView1.Nodes.Add(drive.Name);
+                        treeNode.Tag = drive.RootDirectory.FullName;
+                        treeNode.ImageIndex = 0;
+                        treeNode.Nodes.Add("");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При загрузке дисков произошла ошибка: " + ex.Message);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                TreeNode selectedNode = e.Node;
+                selectedNode.Nodes.Clear();
+                string path = (string)selectedNode.Tag;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (var dir in directoryInfo.GetDirectories())
+                {
+                    TreeNode node = selectedNode.Nodes.Add(dir.Name);
+                    node.Tag = dir.FullName;
+
+                    node.ImageIndex = 1;
+                    node.Nodes.Add("");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При развертывании узла произошла ошибка: " + ex.Message);
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                string path = (string)e.Node.Tag;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.ImageIndex = 2;
+                    listView1.Items.Add(item);
+                }
+                DriveInfo driveInfo = new DriveInfo(Path.GetPathRoot(path));
+                label1.Text = string.Format("Drive: {0}", path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При выборе узла произошла ошибка: " + ex.Message);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    string path = (string)listView1.SelectedItems[0].Tag;
+                    if (File.Exists(path))
+                    {
+                        System.Diagnostics.Process.Start(path);
+                    }
+                    else if (Directory.Exists(path))
+                    {
+                        treeView1.SelectedNode = FindNodeByPath(treeView1.Nodes, path);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный элемент не является файлом или папкой.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При открытии файла произошла ошибка: " + ex.Message);
+            }
+        }
+        private TreeNode FindNodeByPath(TreeNodeCollection nodes, string path)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if ((string)node.Tag == path)
+                {
+                    return node;
+                }
+                TreeNode found = FindNodeByPath(node.Nodes, path);
+                if (found != null)
+                {
+                    return found;
+                }
+            }
+            return null;
+        }
+        private void RefreshTreeNode(TreeNode node)
+        {
+            node.Nodes.Clear();
+            string path = (string)node.Tag;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                TreeNode newNode = new TreeNode(dir.Name);
+                newNode.Tag = dir.FullName;
+                newNode.ImageIndex = 1;
+                newNode.Nodes.Add("");
+                node.Nodes.Add(newNode);
+            }
+        }
+        private void RefreshListView(string path)
+        {
+            listView1.Items.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                ListViewItem item = new ListViewItem(dir.Name);
+                item.SubItems.Add("Папка");
+                item.SubItems.Add(dir.LastWriteTime.ToString());
+                item.ImageIndex = 1;
+                item.Tag = dir.FullName;
+                listView1.Items.Add(item);
+            }
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(file.Name);
+                item.SubItems.Add(file.Length.ToString());
+                item.SubItems.Add(file.LastWriteTime.ToString());
+                item.ImageIndex = 2;
+                item.Tag = file.FullName;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode == null) return;
+
+            string newFolderName = Interaction.InputBox("Введите имя новой папки.", "Создать новую папку.", "Новая папка");
+            if (string.IsNullOrEmpty(newFolderName)) return;
+
+            string newPath = Path.Combine((string)treeView1.SelectedNode.Tag, newFolderName);
+
+            try
+            {
+                Directory.CreateDirectory(newPath);
+                RefreshTreeNode(treeView1.SelectedNode);
+                RefreshListView((string)treeView1.SelectedNode.Tag);
+                MessageBox.Show("Папка успешно создана! ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при создании папки: " + ex.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = treeView1.SelectedNode.FullPath + "\\";
+            string newFileName = Interaction.InputBox("Новое имя файла.", "Создать новый файл.", "новое имя");
+
+            StreamWriter st = File.CreateText(path + newFileName + ".txt");
+            st.Close();
+
+            listView1.Items.Add(newFileName + ".txt", 2);
+            MessageBox.Show("Текстовый файл успешно создан! ");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string oldfile = treeView1.SelectedNode.FullPath + "\\" + listView1.FocusedItem.Text;
+                string newName = Interaction.InputBox("Новое имя файла.", "Переименуйте файл.", "новое имя");
+                if (string.IsNullOrEmpty(newName))
+                    return;
+
+                FileSystem.Rename(oldfile, treeView1.SelectedNode.FullPath + "\\" + newName + ".txt");
+
+                listView1.FocusedItem.Remove();
+                listView1.Items.Add(newName + ".txt", 2);
+                Validate();
+                MessageBox.Show("Текстовый файл успешно переименован! ");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string path = (string)listView1.SelectedItems[0].Tag;
+
+                try
+                {
+                    if (Directory.Exists(path))
+                    {
+                        Directory.Delete(path, true);
+                    }
+                    else if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    RefreshTreeNode(treeView1.SelectedNode);
+                    RefreshListView((string)treeView1.SelectedNode.Tag);
+                    MessageBox.Show("Ярлык успешно удалён! ");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при удалении: " + ex.Message);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
